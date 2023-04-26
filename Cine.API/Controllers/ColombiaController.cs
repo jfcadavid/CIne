f8110c09751ee8ca263f0b282a28d2ef67769f97
jfcadavid <juanfelipecad1216@gmail.com>
2023-04-26 11:52:57 -0500

    COMMIT COLOMBIABRAZOR

---
+++ Cine.API/Controllers/ColombiaController.cs
@@ -0,0 +1,34 @@
+﻿using Cine.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cine.Shared.Entities;
+
+namespace Stores.API.Controllers
+{
+    [ApiController]
+    [Route("/api/colombia")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Colombia colombia)
+        {
+            _context.Add(colombia);
+            await _context.SaveChangesAsync();
+            return Ok(colombia);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Colombia.ToListAsync());
+        }
+
+    }
+}
---
+++ Cine.Web/Repositories/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using Cine.WEB.Repositories;
+
+namespace Cine.Web.Repositories
+{
+    
+        public interface IRepository
+
+    {
+
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+        Task<HttpResponseWrapper<object>> Delete(string url);
+
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
+
+
+    }
+
+}
+
+
