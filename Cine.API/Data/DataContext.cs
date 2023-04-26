b1f0fe7280f3462ab0f4086e5b573473ef7f47bd
jfcadavid <juanfelipecad1216@gmail.com>
2023-04-26 10:58:37 -0500

    Commit cine migration

---
+++ Cine.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cine.Shared.Entities;
+
+
+namespace Cine.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Colombia> Colombia { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Colombia>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
